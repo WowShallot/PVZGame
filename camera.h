b562641030e0c7d22ee270327fc28f74c6a2977e
WowShallot <143866924+WowShallot@users.noreply.github.com>
2025-05-30 15:56:42 +0800

    在camera类中引入定时器，并添加抖动效果，在menu_scene中开始测试摄像机抖动效果

--- camera.h
+++ camera.h
@@ -2,13 +2,32 @@
 #define _CAMERA_H_
 
 #include "vector2.h"
+#include "timer.h"
 
 class Camera
 {
 public:
-	Camera() = default;
+	Camera()
+	{
+		timer_shake.set_one_shot(true); //设置单次触发
+		timer_shake.set_callback([&]()  //设置回调函数 取消抖动状态 并 重置摄像机位置
+			{
+				is_shaking = false;
+				reset();
+			});
+	}
 	~Camera() = default;
 
+	//用于设置摄像机开始抖动的属性
+	//               抖动强度    抖动持续时间
+	void shake(float strength, int duration)
+	{
+		is_shaking = true;
+		shaking_strength = strength;
+		//设置结束抖动状态的定时器
+		timer_shake.set_wait_time(duration);//等待时间为抖动持续时间
+		timer_shake.restart();              //重新开始计时
+	}
 	//返回摄像机位置的常量引用
 	const Vector2& get_position() const
 	{
@@ -24,12 +43,20 @@ public:
 
 	void on_update(int delta)
 	{
-		const Vector2 speed = { -0.35f, 0 }; //给摄像机一个水平向左的速度
-		position += speed * (float)delta;    //让摄像机随着时间逐渐向左移动
+		timer_shake.on_update(delta);
+
+		if (is_shaking) //若摄像机正在抖动，则将摄像机位置的x、y值在2抖动强度为半径的圆内随机取值
+		{
+			position.x = (-50 + rand() % 100) / 50.0f * shaking_strength;
+			position.y = (-50 + rand() % 100) / 50.0f * shaking_strength;
+		}
 	}
 
 private:
-	Vector2 position;
+	Vector2 position;			// 摄像机位置
+	Timer timer_shake;			// 摄像机抖动定时器
+	bool is_shaking = false;	// 摄像机是否正在抖动
+	float shaking_strength = 0; // 摄像机抖动幅度
 };
 
 
